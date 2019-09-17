1085dec5c98df08335b0425c3be07605d7bb773f
Leon Moore <lmoore0621@gmail.com>
2019-09-17 15:27:34 -0500

    update changes

--- Detect-PDF-Is-Password-Proected-master/DetectPassword/Program.cs
+++ Detect-PDF-Is-Password-Proected-master/DetectPassword/Program.cs
@@ -13,10 +13,10 @@ namespace DetectPassword
         {
             //776313
             //Resume
-            var wordFile = @"\\dwshome-a.homeoffice.wal-mart.com\dwsuserdata$\vn5007u\Desktop\ChangeRequests\CHG0504240_Validation Plan_QMS.docx";
-            var wordFileProtected = @"\\dwshome-a.homeoffice.wal-mart.com\dwsuserdata$\vn5007u\Desktop\ChangeRequests\CHG0454645_Validation Plan.docx";
-            var pdfFileProtected = @"C:\Toolbox\Source\Repos\Detect-PDF-Is-Password-Proected-master\Detect-PDF-Is-Password-Proected-master\DetectPassword\bin\776313.pdf";
-            var pdfFileNotProtected = @"\\dwshome-a.homeoffice.wal-mart.com\dwsuserdata$\vn5007u\Desktop\Notes\QMS Database.pdf";
+            var wordFile = @"c";
+            var wordFileProtected = @"c";
+            var pdfFileProtected = @"Cc";
+            var pdfFileNotProtected = @"cc";
 
             var passwordProtectedPDF = false;
             var isPasswordProtectedDoc = false;
