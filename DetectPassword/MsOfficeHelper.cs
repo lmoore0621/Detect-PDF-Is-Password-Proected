12fd2e8d0765e8b5a21919769603f438bf2fcf16
Leon Moore <lmoore0621@gmail.com>
2019-09-17 10:38:10 -0500

    Add files via upload

---
+++ DetectPassword/MsOfficeHelper.cs
@@ -0,0 +1,65 @@
+﻿using iTextSharp.text.exceptions;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectPassword
+{
+    public static class MsOfficeHelper
+    {
+        public static bool IsPasswordProtected(string pdfFile)
+        {
+            var isProtected = false;
+            var isPdf = IsPdf(pdfFile);
+
+            if (pdfFile != null && isPdf)
+            {
+                try
+                {
+                    PdfReader pdfReader = new PdfReader(pdfFile);
+                }
+                catch (BadPasswordException)
+                {
+                    isProtected = true;
+                }
+            }
+            return isProtected;
+        }
+
+        public static string PdfProtectionErrorMessage(bool pdfIsProtected)
+        {
+            var message = "";
+            if (pdfIsProtected)
+            {
+                message = "This File is password Protected, please upload a pdf document that is not password protected.";
+            }
+            return message;
+        }
+
+        private static bool IsPdf(string path)
+        {
+            //ÐÏ.à¡±.á
+            var pdfString = "%PDF-";
+            var pdfBytes = Encoding.ASCII.GetBytes(pdfString);
+            var len = pdfBytes.Length;
+            var buf = new byte[len];
+            var remaining = len;
+            var pos = 0;
+            using (var f = File.OpenRead(path))
+            {
+                while (remaining > 0)
+                {
+                    var amtRead = f.Read(buf, pos, remaining);
+                    if (amtRead == 0) return false;
+                    remaining -= amtRead;
+                    pos += amtRead;
+                }
+            }
+            return pdfBytes.SequenceEqual(buf);
+        }
+    }
+}
---
+++ DetectPassword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //776313
+            //Resume
+            string nonPdfFile = @"C:\Users\lmoor\OneDrive\Desktop\Resume.docx";
+            string pdfFileProtected = @"C:\Users\lmoor\OneDrive\Desktop\776313.pdf";
+            string pdfFileNotProtected = @"C:\Users\lmoor\OneDrive\Desktop\Resume.pdf";
+
+            bool passwordProtectedPDF = MsOfficeHelper.IsPasswordProtected(pdfFileProtected);
+
+            if (passwordProtectedPDF)
+            {
+                string pdfProtectionErrorMessage = MsOfficeHelper.PdfProtectionErrorMessage(passwordProtectedPDF);
+                Console.WriteLine(pdfProtectionErrorMessage);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(passwordProtectedPDF);
+                Console.ReadLine();
+            }
+        }
+    }
+}
